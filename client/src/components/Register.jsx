2693b644847d8e8196b3663069bd012e6d87910c
satish kumar <satishchandala834@gmail.com>
2025-11-28 14:49:54 +0530

    2nd commit

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -26,7 +26,7 @@ function Register({ setAuth }) {
     setLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', formData);
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, formData);
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('user', JSON.stringify(response.data.user));
       setAuth(true);
