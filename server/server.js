595cbeedc3919628696851055877dc688161fa53
satish kumar <satishchandala834@gmail.com>
2025-11-28 15:13:34 +0530

    4th

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://loginsystem-black.vercel.app',
   credentials: true
 }));
 app.use(express.json());
